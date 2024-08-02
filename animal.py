5947134a74b431df726c51b3a7b7ba7170cfc71c
cita <juricek.chen@gmail.com>
2024-08-02 16:01:32 +0800

    Add dog function!

--- animal.py
+++ animal.py
@@ -3,8 +3,13 @@ import sys
 def default():
     print('hello')
 
+def dog():
+    print('i am dog!')
 def main():
-    default()
+    if sys.argv[1]=='dog':
+        dog()
+    else:
+        default()
 
 if __name__ == '__main__':
     main()
