2bca66efd40b6494288d033344e314f5af144ac6
cita <juricek.chen@gmail.com>
2024-08-02 15:54:27 +0800

    Add cat function!

--- animal.py
+++ animal.py
@@ -3,8 +3,14 @@ import sys
 def default():
     print('hello')
 
+def cat():
+    print('i am cat!')
+
 def main():
-    default()
+    if sys.argv[1] == 'cat':
+        cat()
+    else:
+        default()
 
 if __name__ == '__main__':
     main()
